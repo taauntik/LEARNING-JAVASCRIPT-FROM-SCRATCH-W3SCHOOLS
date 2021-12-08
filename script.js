e6d56550d0b6d2223ea38d8f6220ca001d53850a
taauntik <devauntik@gmail.com>
2021-12-09 01:17:00 +0600

    [ADD] almost done this chapter

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello world");
+// JS Where To
+
+/*
+*   Javascript Functions and Events: [2nd]
+
+    A Javascript [function] is a block of Javascript code, that can be executed
+    when "called" for.
+
+    For example, a function can be called when an event occurs, like when the
+    user click a button.
+
+    * We will learn much more about functions and events in later chapters. (in w3schools)
+*/
+
+/*
+    * Javascript in <head> or <body> [3rd]
+
+    You can place any number of scripts in HTML document.
+
+    Scripts can be placed in the <body>, or in the <head> section of an HTML
+    page, or both.
+
+    * To view the 4th section please go to index2.html
+*/
